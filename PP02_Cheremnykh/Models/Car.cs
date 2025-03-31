3b3e8de83f9952b7f897172a31eef60c24fbe640
artem <artem@FATT>
2025-03-31 22:11:32 +0500

    Добавьте файлы проекта.

---
+++ PP02_Cheremnykh/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP02_Cheremnykh.Models
+{
+    public class Car
+    {
+        int Id {  get; set; }
+        public string Name { get; set; }
+        public string Marks { get; set; }
+        public string Gos_Number {  get; set; }
+        public string Voditel {  get; set; }
+
+    }
+}
